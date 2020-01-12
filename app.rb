bcd43094ca5d6e4d1ce0712464166c9d1b5d441b
Vasya Kuzmik <theblackarrovv@gmail.com>
2020-01-12 18:55:20 +0200

    Hotfix master

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'roda'
 require 'fast_jsonapi'
+require_relative 'db/database'
 require_relative 'events/money_event'
 Dir[File.join(__dir__, 'events', '*.rb')].sort.each { |file| require file }
 Dir[File.join(__dir__, 'validators', '*.rb')].sort.each { |file| require file }
