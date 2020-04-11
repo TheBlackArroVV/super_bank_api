d1d7e767f8ed7828d729119a1af3f00f2366e3c9
Vasya Kuzmik <theblackarrovv@gmail.com>
2020-04-11 15:28:10 +0300

    Add category routes

---
+++ events/category.rb
@@ -0,0 +1,20 @@
+class Category
+  attr_reader :name
+
+  def initialize(name:)
+    @name = name
+  end
+
+  def valid?
+    return false if name.nil? || name.empty?
+    return false if Database.instance.categories.map {|el| el[:name]}.include?(name)
+
+    true
+  end
+
+  def save_to_db
+    Database.instance.categories.insert(
+      name: name
+    )
+  end
+end
--- events/money_event.rb
+++ events/money_event.rb
@@ -7,9 +7,10 @@ class MoneyEvent
     to_s
   end
 
-  def initialize(amount:, currency:)
+  def initialize(amount:, currency:, category_id:)
     @amount = amount
     @currency = currency.downcase
+    @category_id = category_id
   end
 
   def type
@@ -21,8 +22,13 @@ class MoneyEvent
   end
 
   def save_to_db
-    Database.instance.events.insert(amount: amount, currency: currency, type: type)
+    Database.instance.events.insert(
+      amount: amount,
+      currency: currency,
+      type: type,
+      category_id: category_id
+    )
   end
 
-  attr_reader :amount, :currency
+  attr_reader :amount, :currency, :category_id
 end
